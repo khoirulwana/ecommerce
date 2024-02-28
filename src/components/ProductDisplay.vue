4a0b39d4b6795a170d2340f816ba5f0fba3ddfe6
khoirulwana <khoirul.wana@gmail.com>
2024-02-29 00:39:11 +0700

    updated styling, now background of main container is changes according to page section(men, women, unavailable)

--- src/components/ProductDisplay.vue
+++ src/components/ProductDisplay.vue
@@ -1,6 +1,39 @@
 <template>
+  <!--
+        element will only be rendered (v-if="isProductAvailable") when isProductAvailable is true, and it will be hidden when isProductAvailable is false
+      -->
   <div class="container" v-if="productData">
-    <div class="product">
+    <div class="bg-container">
+      <img
+        src="../assets/powder-blue-web-solid-color-background.jpg"
+        alt=""
+        class="bg-img"
+        v-if="isMensClothing"
+      />
+      <img
+        src="../assets/queen-pink-solid-color-background.jpg"
+        alt=""
+        class="bg-img"
+        v-if="isWomensClothing"
+      />
+      <img
+        src="../assets/light-gray-solid-color-background.jpg"
+        alt=""
+        class="bg-img"
+        v-if="!isProductAvailable"
+      />
+    </div>
+    <div class="unavailable-page" v-if="!isProductAvailable">
+      <p class="unavailable-message">This product is unavailable to show</p>
+      <button class="next-button1" @click="getNextProduct">Next Product</button>
+    </div>
+    <div
+      class="product"
+      :class="{
+        'unavailable-page': !isProductAvailable,
+      }"
+      v-if="isProductAvailable"
+    >
       <div class="product-picture">
         <img :src="productData.image" alt="" class="product-preview" />
       </div>
@@ -15,13 +48,17 @@
           {{ productData.title }}
         </h2>
         <div class="category-rating">
-          <p class="category">{{ productData.category }}</p>
+          <p class="category">
+            {{ productData.category }}
+          </p>
           <p class="rating">
-            {{ productData.rating.rate + "/" + productData.rating.count }}
+            {{ productData.rating.rate + "/" + 5 }}
           </p>
         </div>
         <div class="description-container">
-          <p class="description">{{ productData.description }}</p>
+          <p class="description">
+            {{ productData.description }}
+          </p>
         </div>
         <p
           class="price"
@@ -68,9 +105,14 @@ export default {
     };
   },
   computed: {
+    isProductAvailable() {
+      return this.isMensClothing || this.isWomensClothing;
+    },
+
     isMensClothing() {
       return this.productData && this.productData.category === "men's clothing";
     },
+
     isWomensClothing() {
       return (
         this.productData && this.productData.category === "women's clothing"
@@ -104,8 +146,12 @@ export default {
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap");
+
 .container {
-  background-color: #d6e6ff;
+  background-color: #ffffff;
+  color: #1e1e1e;
+  font-family: "Poppins", sans-serif;
   width: 100%;
   height: 100vh;
   position: absolute;
@@ -123,6 +169,56 @@ export default {
   border-radius: 10px;
   display: grid;
   grid-template-columns: 1fr 1fr;
+  padding: 10px;
+  box-shadow: 0 3px 10px rgb(0 0 0 / 0.5);
+  z-index: 1;
+}
+
+.container .bg-container {
+  width: 100%;
+  height: 71%;
+  position: absolute;
+  top: 0;
+  left: 0;
+}
+
+.container .bg-img {
+  width: 100%;
+  height: 100%;
+}
+
+.container .unavailable-page {
+  background-color: #ffffff;
+  width: 70%;
+  height: 80%;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-family: "Poppins", sans-serif;
+  font-weight: 400;
+  padding: 10px;
+  box-shadow: 0 3px 10px rgb(0 0 0 / 0.5);
+  background-image: url(../assets/../assets/sad-512-mod-s.png);
+  background-size: cover;
+  background-position: center;
+  z-index: 1;
+}
+
+.container .unavailable-page .next-button1 {
+  background-color: #ffffff;
+  border: 3px solid #1e1e1e;
+  color: #3f3f3f;
+  width: 50%;
+  height: 35px;
+  border-radius: 5px;
+  cursor: pointer;
+  font-weight: 800;
+}
+
+.container .unavailable-page .unavailable-message {
+  color: #1e1e1e;
 }
 
 .container .product .product-picture {
@@ -134,29 +230,53 @@ export default {
 }
 
 .container .product .product-picture .product-preview {
-  width: 80%;
-  height: 80%;
+  width: 300px;
+  height: 400px;
 }
 
 .container .product .product-detail {
-  width: 90%;
-  height: 90%;
+  width: 95%;
+  height: 95%;
   display: flex;
   flex-direction: column;
   text-align: left;
+  padding: 5px;
+}
+
+.container .product .product-detail .product-name {
+  height: 100px;
 }
 
 .container .product .product-detail .category-rating {
   display: flex;
   align-items: baseline;
   justify-content: space-between;
+  margin-bottom: -15px;
+}
+
+.container .product .product-detail .category-rating .category {
+  font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+    "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+  font-size: 14px;
+  font-weight: 400;
 }
 
 .container .product .product-detail .description-container {
   width: 100%;
-  height: 40%;
+  height: 200px;
   border-top: 1px solid #dcdcdc;
   border-bottom: 1px solid #dcdcdc;
+  overflow: scroll;
+  padding: 5px;
+  font-family: "Lucida Sans", "Lucida Sans Regular", "Lucida Grande",
+    "Lucida Sans Unicode", Geneva, Verdana, sans-serif;
+  font-size: 16px;
+  font-weight: 400;
+}
+
+.container .product .product-detail .price {
+  margin: 8px 0;
+  font-weight: 800;
 }
 
 .container .action-button {
@@ -170,6 +290,7 @@ export default {
   height: 35px;
   border-radius: 5px;
   cursor: pointer;
+  font-weight: 800;
 }
 
 .container .action-button .next-button {
@@ -178,6 +299,7 @@ export default {
   height: 35px;
   border-radius: 5px;
   cursor: pointer;
+  font-weight: 800;
 }
 
 .blue-text {
